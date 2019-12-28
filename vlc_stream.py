0fac453ceef78ecc810d0c32744816b307e37cc5
AzhaarMohideen <azhaarm94@gmail.com>
2019-12-28 10:39:18 +0530

    initial commit made vlc_stream

---
+++ vlc_stream.py
@@ -0,0 +1,12 @@
+import os
+import sys
+ip = sys.argv[1]
+user = sys.argv[2]
+passw = sys.argv[3]
+s_type = sys.argv[4]
+command = "rtsp://" + str(user) + ":" + str(passw) + "@" + str(ip) + ":554/cam/realmonitor?channel=1&subtype=" + str(s_type)
+#rtsp://admin:cltadmin12@192.168.1.121:554/cam/realmonitor?channel=1&subtype=1
+command_s = "vlc "
+command_t = command_s + "\"" + command + "\""
+print(command_t)
+os.system(command_t)
