626295666e28dbba33b94dca3608c3d056c99f12
AzhaarMohideen <azhaarm94@gmail.com>
2020-01-17 12:54:56 +0530

    added ascii art

--- vlc_stream.py
+++ vlc_stream.py
@@ -1,9 +1,14 @@
 import os
 import sys
+import time
+from dep import name as nm
 ip = sys.argv[1]
 user = sys.argv[2]
 passw = sys.argv[3]
 s_type = sys.argv[4]
+nm.display_azhaar()
+nm.display_clt()
+time.sleep(5)
 command = "rtsp://" + str(user) + ":" + str(passw) + "@" + str(ip) + ":554/cam/realmonitor?channel=1&subtype=" + str(s_type)
 #rtsp://admin:cltadmin12@192.168.1.121:554/cam/realmonitor?channel=1&subtype=1
 command_s = "vlc "
